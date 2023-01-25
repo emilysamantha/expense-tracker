ae59a95c35293b4ee2f3f5b600ffcdb6c3b83cbc
emilysamantha <emilyzarry@gmail.com>
2023-01-25 11:41:28 +1100

    Create Transaction schema

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 const app = express();
 
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
 
 const transactions = require("./routes/transactions");
 
