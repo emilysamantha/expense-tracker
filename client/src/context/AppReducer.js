c5e51025b850f18b1bb5382d4a3c41c43640c8f4
emilysamantha <emilyzarry@gmail.com>
2023-01-25 14:44:47 +1100

    Change to _id

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -1,4 +1,6 @@
 export default (state, action) => {
+  // Changes the state based on the dispatch type
+  // Takes the response and passes it down to each component
   switch (action.type) {
     case "GET_TRANSACTIONS":
       return {
@@ -10,7 +12,7 @@ export default (state, action) => {
       return {
         ...state,
         transactions: state.transactions.filter(
-          (transaction) => transaction.id !== action.payload
+          (transaction) => transaction._id !== action.payload
         ),
       };
     case "ADD_TRANSACTION":
