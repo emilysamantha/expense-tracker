2d9007f8d91f8fe05e7f74e4d064fd5a63024eee
emilysamantha <emilyzarry@gmail.com>
2023-01-25 15:13:05 +1100

    Add numberWIthCommas

--- client/src/components/IncomeExpenses.js
+++ client/src/components/IncomeExpenses.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import { numberWithCommas } from "../utils/format";
 
 const IncomeExpenses = () => {
   const { transactions } = useContext(GlobalContext);
@@ -19,13 +20,13 @@ const IncomeExpenses = () => {
       <div>
         <h4>Income</h4>
         <p id="money-plus" className="money plus">
-          +${income}
+          +${numberWithCommas(income)}
         </p>
       </div>
       <div>
         <h4>Expenses</h4>
         <p id="money-minus" className="money minus">
-          -${expenses}
+          -${numberWithCommas(expenses)}
         </p>
       </div>
     </div>
